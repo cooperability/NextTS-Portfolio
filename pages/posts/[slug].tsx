75cbb67552492959f2f4a8e0583d0ee41740fc72
Cooper Reed <cooperability@gmail.com>
2025-05-03 14:31:38 -0400

    Linters
    -Added eslint and prettier for manual commands plus documentation
    -Added husky and lint-staged, for an automatic pre-commit linting hook
    -cross-env added to set cross-platform env vars for scripts

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,24 +1,23 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
-import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
-import { serialize } from 'next-mdx-remote/serialize';
-import matter from 'gray-matter';
-import fs from 'fs';
-import path from 'path';
-import Layout from "../../components/layout";
-import Head from "next/head";
-import Date from "../../components/date";
-import utilStyles from "../../styles/utils.module.css";
-import ToggleDropdown from "../../components/ToggleDropdown";
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
+import matter from 'gray-matter'
+import fs from 'fs'
+import path from 'path'
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import ToggleDropdown from '../../components/ToggleDropdown'
 
 type Props = {
-  source: MDXRemoteSerializeResult;
-  frontMatter: { [key: string]: any };
-};
+  source: MDXRemoteSerializeResult
+  frontMatter: { [key: string]: any }
+}
 
-// Define the components mapping
 const components = {
   ToggleDropdown,
-};
+}
 
 export default function PostPage({ source, frontMatter }: Props) {
   return (
@@ -38,33 +37,33 @@ export default function PostPage({ source, frontMatter }: Props) {
         )}
       </article>
     </Layout>
-  );
+  )
 }
 
-const POSTS_PATH = path.join(process.cwd(), 'posts');
+const POSTS_PATH = path.join(process.cwd(), 'posts')
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = fs
     .readdirSync(POSTS_PATH)
     .map((path) => path.replace(/\.mdx?$/, ''))
-    .map((slug) => ({ params: { slug } }));
+    .map((slug) => ({ params: { slug } }))
 
   return {
     paths,
     fallback: false,
-  };
-};
+  }
+}
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   if (!params?.slug) {
-    throw new Error('Slug is required');
+    throw new Error('Slug is required')
   }
-  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`);
-  const source = fs.readFileSync(postFilePath);
+  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`)
+  const source = fs.readFileSync(postFilePath)
 
-  const { content, data } = matter(source);
+  const { content, data } = matter(source)
 
-  let mdxSource = null;
+  let mdxSource = null
   if (postFilePath.endsWith('.mdx')) {
     mdxSource = await serialize(content, {
       mdxOptions: {
@@ -72,7 +71,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         rehypePlugins: [],
       },
       scope: data,
-    });
+    })
   }
 
   return {
@@ -80,5 +79,5 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       source: mdxSource,
       frontMatter: data,
     },
-  };
-};
+  }
+}
