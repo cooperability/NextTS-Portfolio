eac51c543fe5d2a71775aa5e5dc3912898b04e7d
Cooper Reed <cooperability@gmail.com>
2025-05-15 21:57:25 -0400

    Re-layout Demo Stack Icons

--- src/components/ActiveIcon.tsx
+++ src/components/ActiveIcon.tsx
@@ -5,21 +5,35 @@ interface ActiveIconProps {
   href: string
   imgSrc: string
   variant?: 'default' | 'social'
+  size?: 'default' | 'small'
 }
 
 const ActiveIcon: React.FC<ActiveIconProps> = ({
   href,
   imgSrc,
   variant = 'default',
+  size = 'default',
 }) => {
   // Extract filename from imgSrc for alt text
   const altText = imgSrc.substring(imgSrc.lastIndexOf('/') + 1)
 
   // Base class is always hoverImage
   let imageClassName = styles.hoverImage
+  let imageWidth = 50
+  let imageHeight = 50
+
   // Add socialsLink class if variant is social
   if (variant === 'social') {
     imageClassName = `${imageClassName} ${styles.socialsLink}`
+  }
+
+  // Adjust size and styles for 'small' variant
+  if (size === 'small') {
+    imageWidth = 20
+    imageHeight = 20
+    // Optionally, use a different class or modify existing for small icons
+    // For now, we'll just adjust size. We can add specific small icon styles later if needed.
+    imageClassName = styles.inlineIcon // Assuming you might want a different style, or just remove hover
   }
 
   return (
@@ -28,8 +42,8 @@ const ActiveIcon: React.FC<ActiveIconProps> = ({
         src={imgSrc}
         alt={altText}
         className={imageClassName} // Apply combined class names
-        width={50}
-        height={50}
+        width={imageWidth} // Use dynamic width
+        height={imageHeight} // Use dynamic height
       />
     </a>
   )
--- src/pages/demos.tsx
+++ src/pages/demos.tsx
@@ -8,91 +8,129 @@ const Demos: React.FC = () => {
   return (
     <Layout home>
       <section className={styles.headingMd}>
-        <div className={styles.linkRow}>
-          <p>
-            <b>Front End:</b>
-          </p>
-          <ActiveIcon href="https://react.dev/" imgSrc="/images/react.png" />
-          <ActiveIcon href="https://nextjs.org/" imgSrc="/images/nextjs.png" />
-          <ActiveIcon
-            href="https://tailwindcss.com/"
-            imgSrc="/images/css.png"
-          />
-          <ActiveIcon
-            href="https://tailwindcss.com/"
-            imgSrc="/images/tailwind.png"
-          />
-        </div>
         <div>
-          <div>
+          <div className={styles.projectItem}>
             <a href="https://github.com/cooperability/NextTS-Portfolio">
               cooperability.com[💻📱]
             </a>{' '}
             (You are here){' '}
+            <ActiveIcon
+              href="https://react.dev/"
+              imgSrc="/images/react.png"
+              size="small"
+            />
+            <ActiveIcon
+              href="https://nextjs.org/"
+              imgSrc="/images/nextjs.png"
+              size="small"
+            />
+            <ActiveIcon
+              href="https://tailwindcss.com/"
+              imgSrc="/images/tailwind.png"
+              size="small"
+            />
+            <ActiveIcon
+              href="https://nodejs.org/en"
+              imgSrc="/images/njs.png"
+              size="small"
+            />
+            {/* Add TypeScript icon here when available: <ActiveIcon href="https://www.typescriptlang.org/" imgSrc="/images/typescript.png" size="small" /> */}
           </div>
           <div>
             <Link href="/opioid-converter/">Opioid Converter[💻📱]</Link> (2021
-            for Stanford Hospitals)
+            @Stanford Hospitals){' '}
+            <ActiveIcon
+              href="https://react.dev/"
+              imgSrc="/images/react.png"
+              size="small"
+            />
+            <ActiveIcon
+              href="https://tailwindcss.com/"
+              imgSrc="/images/tailwind.png"
+              size="small"
+            />
+            <ActiveIcon
+              href="https://nodejs.org/en"
+              imgSrc="/images/njs.png"
+              size="small"
+            />
+            {/* Add TypeScript icon here when available: <ActiveIcon href="https://www.typescriptlang.org/" imgSrc="/images/typescript.png" size="small" /> */}
           </div>
-          <div>
+          <div className={styles.projectItem}>
             <a href="https://drive.google.com/file/d/1oDYsEytMWbKWmyotBZDIMBzoL5DM1tDV/view?usp=sharing">
-              Personnel Portal (TS, Tailwind, Redux)[📽️]
+              Personnel Portal[📽️]
             </a>
-            (2021 @Edstruments)
+            (2021 @Edstruments){' '}
+            <ActiveIcon
+              href="https://react.dev/"
+              imgSrc="/images/react.png"
+              size="small"
+            />
+            <ActiveIcon
+              href="https://tailwindcss.com/"
+              imgSrc="/images/tailwind.png"
+              size="small"
+            />
+            <ActiveIcon
+              href="https://nodejs.org/en"
+              imgSrc="/images/njs.png"
+              size="small"
+            />
+            {/* Add Redux icon if available and desired */}
           </div>
         </div>
-        <div className={styles.horizLine}></div>
-        <div className={styles.linkRow}>
-          <p>
-            <b>Back End:</b>
-          </p>
+        <div>
+          <div className={styles.projectItem}>
+            <a href="https://github.com/cooperability/nlp_ipynb/">NLP_ipynb</a>{' '}
+            (Ongoing){' '}
+            <ActiveIcon
+              href="https://www.python.org/"
+              imgSrc="/images/python.png"
+              size="small"
+            />
+          </div>
+        </div>
+        <div className={styles.projectItem}>
+          <a href="https://github.com/cooperability/BMX-bookmark-extractor">
+            BookMark eXtractor
+          </a>
+          (Ongoing){' '}
           <ActiveIcon
             href="https://www.python.org/"
             imgSrc="/images/python.png"
+            size="small"
           />
           <ActiveIcon
             href="https://www.djangoproject.com/"
             imgSrc="/images/django.png"
+            size="small"
           />
-          <ActiveIcon href="https://nodejs.org/en" imgSrc="/images/njs.png" />
-        </div>
-        <div>
-          <div>
-            <a href="https://github.com/cooperability/nlp_ipynb/">
-              NLP_ipynb (Python + SpaCy/NLTK/pd etc)
-            </a>{' '}
-            (Ongoing)
-          </div>
-        </div>
-        <div className={styles.horizLine}></div>
-        <div className={styles.linkRow}>
-          <b>Database:</b>
           <ActiveIcon
             href="https://www.postgresql.org/"
             imgSrc="/images/postgresql.png"
+            size="small"
           />
           <ActiveIcon
             href="https://www.mongodb.com/"
             imgSrc="/images/mongodb.png"
+            size="small"
           />
           <ActiveIcon
             href="https://firebase.google.com/"
             imgSrc="/images/firebase.png"
+            size="small"
+          />
+          <ActiveIcon
+            href="https://nodejs.org/en"
+            imgSrc="/images/njs.png"
+            size="small"
           />
         </div>
-        <div>
-          <a href="https://github.com/cooperability/BMX-bookmark-extractor">
-            BookMark eXtractor
-          </a>
-          (Python Hybrid OGM project), a knowledge-tracking tool for your
-          information diet. (Ongoing)
-        </div>
-        <div className={styles.horizLine}></div>
         <ToggleDropdown title="Other Stack Elements I've Worked With">
           <p>
             <b>Lang:</b>
             <br />
-            C++, Rust, Redis
+            C++, Rust, Redis, TypeScript, Redux, Neo4j
             <br />
             <b>Web3:</b>
             <br />
@@ -101,11 +139,11 @@ const Demos: React.FC = () => {
             <b>Data Sci, ML/AI:</b>
             <br />
             Poetry, TensorFlow, PyTorch, Keras, NumPy, Pandas, Scikit-learn,
-            LangChain
+            LangChain, SpaCy, NLTK
             <br />
             <b>DevOps:</b>
             <br />
-            Kubernetes, Heroku, CircleCI, GitLab CI, Linear
+            Kubernetes, Docker, Heroku, CircleCI, GitLab CI, Linear, Vercel
           </p>
         </ToggleDropdown>
       </section>
