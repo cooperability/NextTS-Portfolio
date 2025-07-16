53d5cea9b7c9e46cc45f66e175fd58594b18c4f3
Cooper Reed <cooperability@gmail.com>
2025-07-15 22:57:44 -0400

    cross-platform icons; next-PWA
    Smoothed apple, android, and web iconography
    Enabled next-PWA
    See README for project details and resources

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -20,6 +20,15 @@ export default function Layout({
           <html lang="en" />
           <title>Cooper Reed | Co-Operability</title>
           <link rel="icon" type="image/ico" href="/icon.ico" />
+          <link rel="manifest" href="/icons/site.webmanifest" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Co-Operability" />
           <meta name="og:title" content={siteTitle} />
           <meta name="description" content="Cooper's portfolio website" />
         </Head>
