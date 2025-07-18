9d28313c225f0382b467c50addd8a7664b06ade9
Cooper Reed <cooperability@gmail.com>
2025-07-17 23:47:40 -0400

    Yarn PnP/nodeLinker/Tsconfig fixes
    Fix yarn Pnp Resolution and Tsconfig
    -no more node_modules
    -dependencies resolved from .yarn/cache
    -19 sdk files for IDE integration

--- .yarn/sdks/prettier/index.cjs
+++ .yarn/sdks/prettier/index.cjs
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
 
-const { existsSync } = require(`fs`)
-const { createRequire, register } = require(`module`)
-const { resolve } = require(`path`)
-const { pathToFileURL } = require(`url`)
+const {existsSync} = require(`fs`);
+const {createRequire, register} = require(`module`);
+const {resolve} = require(`path`);
+const {pathToFileURL} = require(`url`);
 
-const relPnpApiPath = '../../../.pnp.cjs'
+const relPnpApiPath = "../../../.pnp.cjs";
 
-const absPnpApiPath = resolve(__dirname, relPnpApiPath)
-const absUserWrapperPath = resolve(__dirname, `./sdk.user.cjs`)
-const absRequire = createRequire(absPnpApiPath)
+const absPnpApiPath = resolve(__dirname, relPnpApiPath);
+const absUserWrapperPath = resolve(__dirname, `./sdk.user.cjs`);
+const absRequire = createRequire(absPnpApiPath);
 
-const absPnpLoaderPath = resolve(absPnpApiPath, `../.pnp.loader.mjs`)
-const isPnpLoaderEnabled = existsSync(absPnpLoaderPath)
+const absPnpLoaderPath = resolve(absPnpApiPath, `../.pnp.loader.mjs`);
+const isPnpLoaderEnabled = existsSync(absPnpLoaderPath);
 
 if (existsSync(absPnpApiPath)) {
   if (!process.versions.pnp) {
     // Setup the environment to be able to require prettier
-    require(absPnpApiPath).setup()
+    require(absPnpApiPath).setup();
     if (isPnpLoaderEnabled && register) {
-      register(pathToFileURL(absPnpLoaderPath))
+      register(pathToFileURL(absPnpLoaderPath));
     }
   }
 }
 
 const wrapWithUserWrapper = existsSync(absUserWrapperPath)
-  ? (exports) => absRequire(absUserWrapperPath)(exports)
-  : (exports) => exports
+  ? exports => absRequire(absUserWrapperPath)(exports)
+  : exports => exports;
 
 // Defer to the real prettier your application uses
-module.exports = wrapWithUserWrapper(absRequire(`prettier`))
+module.exports = wrapWithUserWrapper(absRequire(`prettier`));
